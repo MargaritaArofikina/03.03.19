2cce0846b0c77e403dd43ba8687e79e6a8adf344
MargaritaArofikina <44144804+MargaritaArofikina@users.noreply.github.com>
2019-03-03 15:33:15 +0300

    Update 6!.cpp

--- 6!.cpp
+++ 6!.cpp
@@ -18,44 +18,40 @@ int main() {
    bool correct;
      int m1 = 0,s1 = 0,t1 = 0,m2 = 0, s2 = 0, t2 = 0;
      for (int i = 0; i < n; i++){
-     if (arr[i] == ")") m1++;
-     if (arr[i] == "(") m2++;
-     if (arr[i] == "[") s1++;
-     if (arr[i] == "]") s2++;
-     if (arr[i] == "{") t1++;
-     if (arr[i] == "}") t2++;
+     if (arr[i] == ')') m1++;
+     if (arr[i] == '(') m2++;
+     if (arr[i] == '[') s1++;
+     if (arr[i] == ']') s2++;
+     if (arr[i] == '{') t1++;
+     if (arr[i] == '}') t2++;
      }
      if (m1 != m2 || s1 != s2 || t1 != t2) {
        correct = 0; 
      } else {
-       for (int i = 0; i < n; i++){
-        if (arr[i] == "("){
+       int i = 0;
+        if (arr[i] == '('){
           for (int j = i; j < n; j++){
-            if (arr[j] == ")"){
+            if (arr[j] == ')'){
              char * arr2 = new char [n - 2];
-             for (int g = 0; g < i; g++){
-               arr2 [g] = arr [g];
-             }
              for (int g = i + 1; g < j; g++){
-               arr2 [g] = arr [g + 1];
+               arr2 [g - 1] = arr [g];
              }
              for (int g = j + 1; g < n; g++){
-               arr2 [g] = arr [g + 2];
+               arr2 [g - 2] = arr [g];
              }
              n -= 2;
              arr = arr2;
+             break;
             }
-            break;
+           
           }
+           
         }
   
-        if (arr[i] == "{"){
+        if (arr[i] == '{'){
           for (int j = i; j < n; j++){
-            if (arr[j] == "}"){
+            if (arr[j] == '}'){
              char * arr2 = new char [n - 2];
-             for (int g = 0; g < i; g++){
-               arr2 [g] = arr [g];
-             }
              for (int g = i + 1; g < j; g++){
                arr2 [g] = arr [g + 1];
              }
@@ -69,13 +65,10 @@ int main() {
           }
         }
 
-        if (arr[i] == "["){
+        if (arr[i] =='['){
           for (int j = i; j < n; j++){
-            if (arr[j] == "]"){
+            if (arr[j] == ']'){
              char * arr2 = new char [n - 2];
-             for (int g = 0; g < i; g++){
-               arr2 [g] = arr [g];
-             }
              for (int g = i + 1; g < j; g++){
                arr2 [g] = arr [g + 1];
              }
@@ -88,7 +81,7 @@ int main() {
             break;
      }
     }
-    }
+    
     if (n == 0) {
       correct = 1;
     } else {
@@ -99,7 +92,7 @@ int main() {
 
 
   if (correct == 0) cout << "incorrect";
-  else cout << "correct";
+  if (correct == 1) cout << "correct";
 
 
 
